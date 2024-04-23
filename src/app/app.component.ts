669bfbba81881fdd44abeba3c1ecbae1617cdac4
CarlosLadino0 <carlosladino.iearm6@gmail.com>
2024-04-23 00:49:43 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { NgForOf } from '@angular/common';
+import { countReset } from 'console';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CardComponent, NgForOf], 
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Progra-web';
+  people = [
+    {
+    name: "Maria",
+    country: "Colombia"
+    },
+    {
+    name: "Alonso",
+    country: "Perú"
+    },
+    {
+    name: "Andres",
+    country: "Ecuador"
+    },
+    {
+    name: "Carlos",
+    country: "Panama"
+    },
+    {
+    name: "Beto",
+    country: "Argentina"
+    }
+]
+}
